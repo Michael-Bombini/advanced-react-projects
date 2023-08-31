2f3cd8210ae25d387dda497e158bfca979e94db5
Michael Bombini <bombini.michael@gmail.com>
2023-09-01 01:20:23 +0200

    completed react portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import Modal from "./components/Modal";
+
 export default function App() {
+  const [show, setShow] = useState(false);
+
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <button onClick={() => setShow(true)}>Open Modal</button>
+      <Modal show={show} closeModal={() => setShow(false)} />
+    </div>
+  );
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+interface Props {
+  show: boolean;
+  closeModal: () => void;
+}
+
+export default function Modal({ show, closeModal }: Props) {
+  if (!show) {
+    return null;
+  }
+  const portalNode = document.getElementById("portal");
+  if (!portalNode) {
+    return null;
+  }
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <div className="modal">
+      <div className="overlay"></div>
+      <div className="content">
+        <h2>Simple Modal</h2>
+        <button onClick={closeModal}>Close Modal</button>
+      </div>
+    </div>,
+    portalNode
+  );
+}
